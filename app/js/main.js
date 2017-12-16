6892384731d6d64b8627b7c030996b11137309e8
Nurbol Sarsenbaev <nurbol.proger@gmail.com>
2017-12-16 08:26:55 +0600

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,12 @@ $(function() {
         items: 3,
         nav: true,
         loop: true,
-        margin: 180,
         smartSpeed: 500,
         responsive: {
             0: { items: 1 },
-            481: { items: 2 },
-            769: { items: 3 }            
+            481: { items: 2, margin: 30 },
+            769: { items: 3, margin: 130 },
+            993: { margin: 180 }          
         },
         navText: ['<i class="arrow-left">', '<i class="arrow-right">'],        
         onChanged: function(e) {
@@ -30,6 +30,31 @@ $(function() {
         loop: true,
         smartSpeed: 500,
         navText: ['<i class="arrow-left">', '<i class="arrow-right">']
+    });
+
+    $(".modal").click(function() {
+        $(this).fadeOut(500);        
+    });
+
+    $(".modal-close").click(function() {
+        $(this).closest('.modal').fadeOut(500);
+    });
+
+    $(".section-services-gallery-item .more").click(function() {
+        var id = $(this).data("id");
+
+        $("#modal-service-" + id).fadeIn(500);
+    });
+
+    var $html = $("html, body");
+
+    $(".header-menu a").click(function(e) {
+        e.preventDefault();
+        var $href = $(this).attr('href');
+        if($href.length > 0) {
+            var top = $href.length == 1 ? 0 : $($href).offset().top;
+            $html.stop().animate({ scrollTop: top }, "slow", "swing");
+        }
     });
 
 });
